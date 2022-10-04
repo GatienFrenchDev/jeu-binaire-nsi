89fe0a0c17fbd4761b4b0bc17167d9ef43e0d937
Gatien <gatieng2005@gmail.com>
2022-10-04 18:38:30 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Jeu Binaire - Gatien G. et Lokendra B.
 
-import pyxel, time
+import pyxel, time, random
+ 
+nb_lignes_grilles = 1
 
 
 class Coins:
@@ -10,8 +12,8 @@ class Coins:
         self.purse = purse
 
     def draw(self):
-        pyxel.blt(self.x, self.y, 0, 0, 0, 6, 7)
-        pyxel.text(self.x+8, self.y+1, f"{self.purse}", 7)
+        pyxel.blt(self.x, self.y, 0, 0, 0, 6, 7, 0)
+        pyxel.text(self.x+8, self.y+1, f"{self.purse+1}", 7)
 
 
 class Monster:
@@ -19,18 +21,47 @@ class Monster:
         self.x = x
         self.y = y
         self.lvl = lvl
+        self.index = random.randint(0, 3)
         self.hp = 3*self.lvl+10
 
     def draw(self):
-        pyxel.blt(self.x, self.y, 1, (self.lvl % 4)*16, (self.lvl // 4)*16, 16, 16)
+        pyxel.text(70, 3, f"lvl {self.lvl}", 7)
+        pyxel.blt(self.x, self.y, 1, self.index*16, (self.lvl // 4)*16, 16, 16, 0)
+    
+    def reset_hp(self):
+        self.hp = self.get_default_hp()
+    
+    def set_new_index(self):
+        self.index = random.randint(0, 3)
+
+    def get_default_hp(self)->int:
+        return 3*self.lvl+10
+
+class HealthBar:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def draw(self, monster: Monster):
+        pyxel.text(self.x+5, self.y-7, f"{monster.hp} hp", 7)
+        pyxel.rect(self.x, self.y, 30, 4, 13)
+        pyxel.rect(self.x, self.y, (monster.hp/monster.get_default_hp())*30, 4, 8)
+        
 
 class Player:
     def __init__(self):
         self.strength = 1
         self.multiplier = 1
+        self.coin_multiplier = 1
 
-    def attack(self, monster: Monster):
+    def attack(self, monster: Monster, coins: Coins):
         monster.hp -= self.strength * self.multiplier
+        if monster.hp < 1:
+            monster.lvl += 1
+            monster.reset_hp()
+            monster.set_new_index()
+            coins.purse += (monster.lvl // 5 + 1)*self.coin_multiplier
+
 
 class App:
     """
@@ -40,19 +71,21 @@ class App:
         pyxel.init(108, 72, "0b1001", 60, display_scale=10)
         pyxel.load("rsc/ressources.pyxres")
         pyxel.mouse(True)
+        pyxel.tilemap(0)
+        self.hp_bar = HealthBar(9, 50)
         self.player = Player()
         self.coin = Coins(3, 2)
-        self.monster = Monster(15, 30, 1)
+        self.monster = Monster(15, 20)
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
-            self.player.attack(self.monster)
-            print(self.monster.hp)
+        if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT) or pyxel.btnp(pyxel.KEY_SPACE):
+            self.player.attack(self.monster, self.coin)
 
     def draw(self):
         pyxel.cls(0)
         self.coin.draw()
+        self.hp_bar.draw(self.monster)
         self.monster.draw()
 
 
